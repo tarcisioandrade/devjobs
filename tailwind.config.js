2223ce7fcd6ae27db5e93673284f5c692879783e
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-10-29 12:14:50 -0300

    structured base design

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{tsx,js,ts,jsx}",
+    "./components/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
