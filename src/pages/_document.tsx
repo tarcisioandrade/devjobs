78a2fdd27f005e9490fe5fed8b6f56b913465706
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-11-08 12:56:46 -0300

    refactor: document and index page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="dark">
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
-import UserProvider from "@contexts/UserContext";
-import Header from "@components/Header";
 import JobsContainer from "@components/JobsContainer";
 
 const Home = () => {
   return (
-    <main className="dark">
-      {/* Header  */}
-      <UserProvider>
-        <Header />
-      </UserProvider>
-
-      {/* JobsContainer */}
-      <JobsContainer /> 
-    </main>
+    <>
+      <JobsContainer />
+    </>
   );
 };
 
