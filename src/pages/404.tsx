341328482c53311f2c380b2367471be459845f3d
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-11-19 15:47:46 -0300

    added page 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { Button } from "flowbite-react";
+import Link from "next/link";
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <main className="flex items-center justify-center h-full">
+      <div>
+        <p className="dark:text-white text-4xl">Página não encontrada.</p>
+        <Link href="/">
+          <Button className="mx-auto mt-4 block">Página Inicial</Button>
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default PageNotFound;
