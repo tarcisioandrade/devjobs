b5a5f7006c94af699335d35682f6a46541817b6d
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-11-24 10:34:21 -0300

    header modifies

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
-import { Navbar } from "flowbite-react";
+import { Button } from "flowbite-react";
 import { Avatar, Dropdown } from "flowbite-react";
-import React from "react";
 import { useUserContext } from "@contexts/UserContext";
 import Link from "next/link";
 
@@ -8,52 +7,59 @@ const Header = () => {
   const { login, user, logout } = useUserContext();
 
   return (
-    <header>
-      <Navbar fluid>
-        <Navbar.Brand href="/">
-          <span className="self-center whitespace-nowrap dark:text-white text-xl font-semibold">
-            DevJobs
-          </span>
-        </Navbar.Brand>
+    <header className="bg-gray-800 min-h-[4rem]">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <Link href="/" className="text-2xl font-semibold text-gray-200">
+          DevJobs
+        </Link>
+        {user ? (
+          <div className="flex md:order-2">
+            <Dropdown
+              arrowIcon={false}
+              inline={true}
+              label={
+                <Avatar alt="User settings" img={user?.avatar} rounded={true} />
+              }
+            >
+              {user && (
+                <Dropdown.Header>
+                  <span className="block text-sm">{`${user.name} ${user.surname}`}</span>
+                  <span className="block truncate text-sm font-medium">
+                    {user.email}
+                  </span>
+                </Dropdown.Header>
+              )}
 
-        <div className="flex md:order-2">
-          <Dropdown
-            arrowIcon={false}
-            inline={true}
-            label={
-              <Avatar alt="User settings" img={user?.avatar} rounded={true} />
-            }
+              <Dropdown.Item>
+                <Link href="/user/jobsapplied">Vagas Aplicadas</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/user/profile">Meu Perfil</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/user/jobpost">Postar Vaga</Link>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <Link href="/user/jobsposted">Vagas Postadas</Link>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              {user ? (
+                <Dropdown.Item onClick={() => logout()}>Sair</Dropdown.Item>
+              ) : (
+                <Dropdown.Item onClick={() => login()}>Login</Dropdown.Item>
+              )}
+            </Dropdown>
+          </div>
+        ) : (
+          <Button
+            size="sm"
+            gradientDuoTone="greenToBlue"
+            onClick={() => login()}
           >
-            {user && (
-              <Dropdown.Header>
-                <span className="block text-sm">{`${user.name} ${user.surname}`}</span>
-                <span className="block truncate text-sm font-medium">
-                  {user.email}
-                </span>
-              </Dropdown.Header>
-            )}
-
-            <Dropdown.Item>
-              <Link href="/user/jobsapplied">Vagas Aplicadas</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/user/profile">Meu Perfil</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/user/jobpost">Postar Vaga</Link>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <Link href="/user/jobsposted">Vagas Postadas</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            {user ? (
-              <Dropdown.Item onClick={() => logout()}>Sair</Dropdown.Item>
-            ) : (
-              <Dropdown.Item onClick={() => login()}>Login</Dropdown.Item>
-            )}
-          </Dropdown>
-        </div>
-      </Navbar>
+            Login
+          </Button>
+        )}
+      </div>
     </header>
   );
 };
