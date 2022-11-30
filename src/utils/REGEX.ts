927c0bb734616a303f33d29a4b2595987dc2fa16
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2022-11-30 10:15:57 -0300

    better regex in patternOnlyLetters

--- src/utils/REGEX.ts
+++ src/utils/REGEX.ts
@@ -1,7 +1,7 @@
 export const patternEmail =
   /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/;
 
-export const patternOnlyLetters = /^[a-zA-Z]+$/;
+export const patternOnlyLetters = /^([A-Za-z]{3,}|[\s]{1}[A-Za-z]{1,})*$/;
 
 export const patternURL =
   /^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
